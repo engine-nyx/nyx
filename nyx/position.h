e51c41e647959879a48d5d96775d12ec69a78cae
Kilian Chung <kilian.chung@web.de>
2026-07-13 16:26:43 +0200

    added do, undo and state_frame struct

--- nyx/position.h
+++ nyx/position.h
@@ -5,11 +5,25 @@
 
 typedef struct
 {
+	square ep;
+	cptype capture;
+	castling_rights castle;
+	unsigned rule50;
+	unsigned plies;
+} state_frame;
+
+typedef struct
+{
 	color stm;
+
+	state_frame *sf;
 
 	bitboard by_color[NUM_COLORS];
 	bitboard by_ptype[NUM_PIECE_TYPES];
 	cptype by_square[NUM_SQUARES];
 } position;
+
+state_frame do_move(position *p, move m);
+void undo_move(position *p, move m, state_frame sf);
 
 #endif // NYX_POSITION_H
