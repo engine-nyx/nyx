8b4668b5b58979dd6a943c7970e4bf8d661b724d
Kilian Chung <kilian.chung@web.de>
2026-07-13 16:25:44 +0200

    added perft

---
+++ nyx/perft.h
@@ -0,0 +1,33 @@
+#ifndef NYX_PERFT_H
+#define NYX_PERFT_H
+
+#include <nyx/movegen.h>
+#include <nyx/position.h>
+#include <nyx/types.h>
+
+static inline uint_fast64_t
+perft(position *p, unsigned depth)
+{
+	if (!depth) return 1;
+
+	uint_fast64_t nodes;
+	move ms[MAX_MOVES], m;
+	size_t num_moves, i;
+	state_frame sf;
+
+	nodes = 0;
+	num_moves = generate_legals(p, ms);
+
+	for (i = 0; i < num_moves; ++i)
+	{
+		m = ms[i];
+
+		sf = do_move(p, m);
+		nodes += perft(p, depth - 1);
+		undo_move(p, m, sf);
+	}
+
+	return nodes;
+}
+
+#endif // NYX_PERFT_H
