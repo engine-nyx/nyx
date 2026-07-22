594388de1849af8a81c50f9821d6dff593a67cb6
Kilian Chung <kilian.chung@web.de>
2026-07-23 00:29:15 +0200

    added empty tt files

---
+++ include/nyx/transposition.h
@@ -0,0 +1,12 @@
+#ifndef NYX_TRANSPOSITION_H
+#define NYX_TRANSPOSITION_H
+
+typedef struct
+{
+	int type;
+} tt_entry;
+
+// maybe add tt config as args here
+void tt_init(void);
+
+#endif // NYX_TRANSPOSITION_H
---
+++ src/transposition.c
@@ -0,0 +1,7 @@
+#include <nyx/transposition.h>
+
+void
+tt_init(void)
+{
+	return;
+}
