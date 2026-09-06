fba1d8598fe1057286b22fee34f09539719d699a
Kilian Chung <kilian.chung@web.de>
2026-09-06 18:22:59 +0200

    added qsearch

--- src/search.c
+++ src/search.c
@@ -24,6 +24,72 @@ tt_skip(tt_entry ent, int alpha, int beta, struct search_state ss)
 }
 
 static int
+quiescence_rec(position *p, int alpha, int beta, time_manager *tm, struct search_state *ss)
+{
+	selector s;
+	move m, best_move;
+	int score, best_score;
+	state_frame sf;
+	tt_entry ent;
+	bool tt_probe_success;
+
+	tt_probe_success = tt_probe(ss->tt, p->key, &ent);
+
+	if (tt_probe_success && tt_skip(ent, alpha, beta, *ss))
+		return ent.score;
+
+	--ss->depth;
+	++ss->nodes;
+
+	s = (selector) { .p=p };
+	best_score = alpha;
+
+	while (!tm_hard_expired(tm, ss) && !is_null_move(m = select_move(&s)))
+	{
+		do_move(p, m, &sf);
+
+		if (sf.material < -oo / 2 || sf.material > oo / 2)
+		{
+			score = -evaluate(p);
+		}
+		else
+		{
+			score = -quiescence_rec(p, alpha, beta, tm, ss);
+		}
+
+		undo_move(p, m);
+
+		if (score > best_score)
+		{
+			best_score = score;
+			best_move = m;
+
+			if (score >= beta) break;
+		}
+	}
+
+	++ss->depth;
+	if (score >= beta)
+	{
+		tt_store(ss->tt, (tt_entry)
+		{
+			.depth=(u8)ss->depth,
+			.best_move=best_move,
+			.key=p->key,
+			.score=best_score,
+			.type=
+			(
+				score >= beta ? FAIL_HIGH :
+				score < alpha ? FAIL_LOW :
+				EXACT
+			),
+		});
+	}
+
+	return best_score;
+}
+
+static int
 search_rec(position *p, int alpha, int beta, time_manager *tm, struct search_state *ss)
 {
 	selector s;
@@ -38,7 +104,9 @@ search_rec(position *p, int alpha, int beta, time_manager *tm, struct search_sta
 	if (tt_probe_success && tt_skip(ent, alpha, beta, *ss))
 		return ent.score;
 
-	if (!ss->depth) return evaluate(p);
+	if (!ss->depth)
+		return -quiescence_rec(p, -beta, -alpha, tm, ss);
+
 	--ss->depth;
 	++ss->nodes;
 
