d3572b4ba4db056fdbd279aa5e7e0e17f867d46d
Kilian Chung <kilian.chung@web.de>
2026-09-08 17:52:20 +0200

    giving overflow to the biggest section

--- test/base.c
+++ test/base.c
@@ -177,10 +177,14 @@ print_group_progress(group_node *group, size_t ran, size_t failures)
 {
 	unsigned i;
 	unsigned green, empty, red;
+	unsigned *biggest;
 
 	green = (unsigned) (((float) (ran - failures) / (float) group->test_count) * BAR_WIDTH);
 	red   = (unsigned) (((float) (failures) / (float) group->test_count) * BAR_WIDTH);
-	empty = BAR_WIDTH - green - red;
+	empty = (unsigned) (((float) (group->test_count - ran) / (float) group->test_count) * BAR_WIDTH);
+
+	biggest = green > red ? (green > empty ? &green : &empty) : (red > empty ? &red : &empty);
+	*biggest += BAR_WIDTH - green - red - empty;
 
 	printf("> Test group %s%-*s %3zu test(s) ran; %3zu failed. ", group->name, (int) (longest_group_name - strlen(group->name) + 1), ":", ran, failures);
 	printf("[\x1b[32m\x1b[7m");
