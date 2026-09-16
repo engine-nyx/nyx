4e9a1bedf71244ae225300fb23e3e3ea2977db2e
Kilian Chung <kilian.chung@web.de>
2026-09-17 01:11:41 +0200

    remove error output for faulty commit

--- tools/release.sh
+++ tools/release.sh
@@ -16,7 +16,7 @@ VERSION_FILE="$ROOT_DIR/include/nyx/version.h"
 echo "#define NYX_VERSION \"$VERSION\"" > "$VERSION_FILE"
 
 git add "$VERSION_FILE"
-git commit -am "Bump version to $VERSION" || true
+git commit -am "Bump version to $VERSION" 2>/dev/null || true
 
 git tag -a "v$VERSION"
 
